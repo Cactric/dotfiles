b4844cd030828bc31909080e061e9f824c65fa4b
Cactric <59334627+Cactric@users.noreply.github.com>
2020-09-03 22:36:37 +0100

    The install script now makes the directories used

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-#Run this in the same working directory as the files
-#ln -rs: -r means relative links
+# Run this in the same working directory as the files
+
+# Create the directories we'll use
+# -p will make the parent directories as well and won't throw an error if they
+# already exist.
+mkdir -p ~/.config/zsh
+mkdir -p ~/.config/nano
+
+# ln -rs: -r means relative links
 
 ln -rs $PWD/zshrc ~/.zshrc
 ln -rs $PWD/aliasrc ~/.config/zsh/aliasrc
