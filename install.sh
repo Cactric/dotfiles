f668e4e283555767e8ea427ec5f4461617070c48
Cactric <59334627+Cactric@users.noreply.github.com>
2022-12-24 15:35:18 +0000

    Use ZDOTDIR in install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}"
 DATA="${XDG_DATA_HOME:-$HOME/.local/share}"
 STATE="${XDG_STATE_HOME:-$HOME/.local/state}"
+ZDOTDIR="${ZDOTDIR:-$HOME}" # Where to put .zshrc and the files zsh looks for. Set in /etc/zsh/zshenv if you want to change it
 
 # Create the directories we'll use
 # -p will make the parent directories as well and won't throw an error if they
@@ -17,7 +18,7 @@ mkdir -p "$CONFIG/neofetch"
 
 # ln -sr: -r means relative links
 
-ln -sr "$PWD/zshrc" "$HOME/.zshrc"
+ln -sr "$PWD/zshrc" "$ZDOTDIR/.zshrc"
 ln -sr "$PWD/aliasrc" "$CONFIG/zsh/aliasrc"
 ln -sr "$PWD/nanorc" "$CONFIG/nano/nanorc"
 ln -sr "$PWD/machine-colours" "$CONFIG/zsh/machine-colours"
